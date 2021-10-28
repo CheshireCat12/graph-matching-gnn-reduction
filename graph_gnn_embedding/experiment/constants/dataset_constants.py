54f559ce4ecb2295baabeaab3b553399b04d7c84
CheshireCat12 <anthony.gillioz@outlook.com>
2021-10-28 17:36:38 +0200

    minor changes

--- graph_gnn_embedding/experiment/constants/dataset_constants.py
+++ graph_gnn_embedding/experiment/constants/dataset_constants.py
@@ -15,4 +15,28 @@ DATASETS = {
             'folder_labels': './data/NCI1/',
         }
     },
+    'mutagenicity': {
+        'coordinator': {
+            'dataset_name': 'mutagenicity',
+            'params_edit_cost': (1.0, 1.0, 1.0, 1.0, 'euclidean'),
+            'folder_dataset': './data/Mutagenicity/',
+            'folder_labels': './data/Mutagencity/',
+        }
+    },
+    'proteins': {
+        'coordinator': {
+            'dataset_name': 'proteins',
+            'params_edit_cost': (1.0, 1.0, 1.0, 1.0, 'euclidean'),
+            'folder_dataset': './data/PROTEINS/',
+            'folder_labels': './data/PROTEINS/',
+        }
+    },
+    'dd': {
+        'coordinator': {
+            'dataset_name': 'dd',
+            'params_edit_cost': (1.0, 1.0, 1.0, 1.0, 'euclidean'),
+            'folder_dataset': './data/DD/',
+            'folder_labels': './data/DD/',
+        }
+    },
 }
